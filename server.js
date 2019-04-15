864fb7566aacefb18fa0ae89e3d716c0b41e0927
shadiboi <saoutabachi@gmail.com>
2019-04-14 20:05:36 -0600

    Commit 4 - show view shows pokemon details

--- server.js
+++ server.js
@@ -3,28 +3,32 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const Pokemon = require('./models/pokemon');
+
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser({extended: true}));
 
 
 
-
-app.use(bodyParser({extended: true}))
-;
 
 app.get('/', (req, res) => {
+    res.send(Pokemon);
+});
+
+app.get('/pokemon', (req, res) => {
     res.render('index.ejs',{
         'pokemon': Pokemon
+    })
+});
+
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs',{
+        'pokemon': Pokemon[req.params.id]
     })
 });
 
 
 
 
-
-
-app.get('/pokemon', (req, res) => {
-    res.send(Pokemon);
-});
 
 
 
