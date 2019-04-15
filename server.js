91923f9ed08c5d57008e0659cb3293988d88c6b8
shadiboi <saoutabachi@gmail.com>
2019-04-14 18:43:51 -0600

    Commit 2 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -2,21 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const pokemon = require('./models/pokemon');
 
 
 
 app.use(bodyParser({extended: true}))
 
-
-
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('hello world');
 });
 
-
-
-
-
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon);
+});
 
 
 
