46d9cb13831f26c2ee643a10a9a85985b13b8a16
shadiboi <saoutabachi@gmail.com>
2019-04-15 22:54:49 -0600

    added new route

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 const Pokemon = require('./models/pokemon');
 
 app.use(express.static(__dirname + '/public'));
+app.use(morgan('short'));
 app.use(bodyParser({extended: true}));
 
 
+app.post('/pokemon', (req,res) => {
+    console.log('made it to create route')
+    console.log(req.body)
+    Pokemon.push(req.body)
+    res.redirect('/pokemon')
 
+})
 
-app.get('/', (req, res) => {
-    res.send(Pokemon);
-});
 
 app.get('/pokemon', (req, res) => {
     res.render('index.ejs',{
@@ -20,11 +25,21 @@ app.get('/pokemon', (req, res) => {
     })
 });
 
-app.get('/pokemon/:id', (req, res) => {
-    res.render('show.ejs',{
-        'pokemon': Pokemon[req.params.id]
+app.get('/pokemon/new', (req,res) => {
+    res.render('new.ejs', {
+    })
+})
+
+app.get('/:id', (req, res) => {
+    res.render('show.ejs', {
+        pokemon: Pokemon[req.params.id]
     })
 });
+
+app.get('/', (req, res) => {
+    res.send(Pokemon);
+});
+
 
 
 
