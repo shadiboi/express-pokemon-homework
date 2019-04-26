caa4dfaec0b0ac05038538b9404766a0347f031a
shadiboi <saoutabachi@gmail.com>
2019-04-26 08:53:30 -0600

    final commit

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 const Pokemon = require('./models/pokemon');
 
+app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('short'));
 app.use(bodyParser({extended: true}));
 
 
-app.post('/pokemon', (req,res) => {
-    console.log('made it to create route')
-    console.log(req.body)
-    Pokemon.push(req.body)
-    res.redirect('/pokemon')
 
+app.put('/pokemon/:id', (req,res) =>{
+    Pokemon[req.params.id] = req.body;
+    res.redirect('/pokemon')
 })
 
+
+app.get('/pokemon/:id/edit', (req,res) => {
+    res.render('edit.ejs', {
+        pokemon: Pokemon[req.params.id],
+        id: req.params.id
+    })
+})
 
 app.get('/pokemon', (req, res) => {
     res.render('index.ejs',{
@@ -30,7 +37,8 @@ app.get('/pokemon/new', (req,res) => {
     })
 })
 
-app.get('/:id', (req, res) => {
+
+app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', {
         pokemon: Pokemon[req.params.id]
     })
@@ -40,7 +48,21 @@ app.get('/', (req, res) => {
     res.send(Pokemon);
 });
 
+app.post('/pokemon', (req,res) => {
+    console.log('made it to create route')
+    console.log(req.body)
+    Pokemon.push(req.body)
+    res.redirect('/pokemon')
 
+})
+
+
+app.delete('/pokemon/:id', (req,res) => {
+    console.log('delete route in par')
+    Pokemon.splice(req.params.id,1);
+    res.redirect('/pokemon');
+    
+})
 
 
 
